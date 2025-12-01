117f05fb7b97c25aff1ece40261d5ca9a8330bc9
karthick <karthicksubraminanam@gmail.com>
2025-12-01 22:08:14 +0530

    Add toggle button state styles with disabled mode

--- src/checkbox/CheckBoxComponent.jsx
+++ src/checkbox/CheckBoxComponent.jsx
@@ -1,10 +1,10 @@
 import './CheckBoxComponent.css';
 
-function CheckBoxComponent({size ='medium', checked, onChange, name, ariaLabel}) {
+function CheckBoxComponent({size ='medium', checked, onChange, name, canDisable = false, ariaLabel}) {
     return (
         <>
             <label className={`checkbox-component ${size}`}>
-                <input checked={checked} aria-label={ariaLabel} type="checkbox" onChange={(e) => onChange?.(e.target.checked)} name={name}/>
+                <input disabled={canDisable} checked={checked} aria-label={ariaLabel} type="checkbox" onChange={(e) => onChange?.(e.target.checked)} name={name}/>
                 <span aria-hidden="true" className="track"></span>
             </label>
         </>
